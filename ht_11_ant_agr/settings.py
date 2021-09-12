dc6360946406b431206eb14402580e9be366c12e
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2021-09-12 12:53:15 +0300

    initial commit

--- ht_11_ant_agr/settings.py
+++ ht_11_ant_agr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d1gea1gp7s)hs+ragj+lnpqnd8h%o6@o%v21un%1o2otyc4nxp'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-d1gea1gp7s)hs+ragj+lnpqnd8h%o6@o%v21un%1o2otyc4nxp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ht_11_ant_agr.apps.Ht_11_ant_agrConfig',
+
 ]
 
 MIDDLEWARE = [
