1c863115070222e24dcf5f6e4b59af1d508e9c0e
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2021-09-14 16:07:55 +0300

    админка пока что готова

--- book_store/admin.py
+++ book_store/admin.py
@@ -3,21 +3,34 @@ from django.contrib import admin
 from .models import Author, Book, Publisher, Store
 
 
+class BookInline(admin.TabularInline):
+    model = Book
+
+
 @admin.register(Author)
-class AuthorInline(admin.ModelAdmin):
+class AuthorAdmin(admin.ModelAdmin):
     fields = ['name', 'age']
+    list_filter = ('age',)
 
 
 @admin.register(Publisher)
-class PublisherInline(admin.ModelAdmin):
+class PublisherAdmin(admin.ModelAdmin):
     fields = ['name']
+    inlines = [
+        BookInline,
+    ]
 
 
 @admin.register(Book)
-class BookInline(admin.ModelAdmin):
-    fields = ['name', 'pages', 'price', 'rating', 'authors', 'publisher', 'pubdate']
+class BookAdmin(admin.ModelAdmin):
+    fields = ('name', ('pages', 'price', 'rating'), 'authors', 'publisher', 'pubdate')
+    date_hierarchy = 'pubdate'
+    list_filter = ('rating',)
+    filter_horizontal = ['authors']
 
 
 @admin.register(Store)
-class StoreInline(admin.ModelAdmin):
+class StoreAdmin(admin.ModelAdmin):
     fields = ['name', 'books']
+    list_filter = ('name',)
+    filter_horizontal = ['books']
