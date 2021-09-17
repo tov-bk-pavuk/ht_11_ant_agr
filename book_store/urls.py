30be43fefb95939475dc9b0790cc97c81d1f5f12
master <tov.bk.pavuk@gmail.com>
2021-09-17 03:55:30 +0300

    готовы базовые шаблоны и представления и один prefetch

--- book_store/urls.py
+++ book_store/urls.py
@@ -5,5 +5,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('books/', views.book_list, name='book_list'),
+    path('books/', views.book_list, name='books'),
+    path('publishers/', views.publishers, name='publishers'),
+    path('stores/', views.stores, name='stores'),
+    path('authors/', views.authors, name='authors'),
 ]
--- book_store/views.py
+++ book_store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Book
+from .models import Author, Book, Publisher, Store
 
 
 def home(request):
@@ -8,6 +8,26 @@ def home(request):
 
 
 def book_list(request):
-    books_query = Book.objects.filter(pk__range=(10, 20))
+    books_query = Book.objects.prefetch_related('authors').filter(pk__range=(5, 10))
+    books_count = Book.objects.all().count()
     return render(request, "book_store/books.html",
-                  context={'books': books_query})
+                  context={'books': books_query,
+                           'amount': books_count})
+
+
+def publishers(request):
+    publishers_query = Publisher.objects.filter(pk__range=(10, 20))
+    return render(request, "book_store/publishers.html",
+                  context={'publishers': publishers_query})
+
+
+def stores(request):
+    stores_query = Store.objects.filter(pk__range=(10, 20))
+    return render(request, "book_store/stores.html",
+                  context={'stores': stores_query})
+
+
+def authors(request):
+    authors_query = Author.objects.filter(pk__range=(10, 20))
+    return render(request, "book_store/authors.html",
+                  context={'authors': authors_query})
