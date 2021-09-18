8616ae80ad70b49caf3b5851778d59e59725a2eb
master <tov.bk.pavuk@gmail.com>
2021-09-18 04:55:02 +0300

    пока лучше так, чем никак

--- book_store/views.py
+++ book_store/views.py
@@ -8,8 +8,6 @@ def home(request):
 
 
 def book_list(request):
-    # books_query = Book.objects.prefetch_related('authors').filter(pk__range=(5, 10))
-    # books_query = Book.objects.prefetch_related('authors').all()
     books_query = Book.objects.all()
     books_count = books_query.count()
     return render(request, "book_store/books.html",
@@ -53,13 +51,13 @@ def stores_detailed(request, id):
 
 
 def authors(request):
-    authors_query = Author.objects.filter(pk__range=(10, 20))
+    authors_query = Author.objects.prefetch_related('book_set').all()
     return render(request, "book_store/authors.html",  # authors_detailed, name='aut_det'),
                   context={'authors': authors_query})  # stores_detailed, name='sto_det'),
 
 
 def authors_detailed(request, id):
     pk = Author.objects.prefetch_related('book_set').get(pk=id)
-    return render(request, 'book_store/detailed_authors.html', context={
+    return render(request, 'book_store/detailed_author.html', context={
         'pk': pk,
     })
